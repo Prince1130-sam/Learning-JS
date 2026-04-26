5410ba2b277d1acf359fe75c8907bfd841ff1cc4
Prince1130-sam <princekuldeep2022@gmail.com>
2026-04-26 18:44:48 +0530

    add IIEF function

---
+++ module_6/IIFE_function.js
@@ -0,0 +1,17 @@
+
+
+
+const fun = ( function (){
+    for(let i=0 ; i <= 10 ; i++){
+        if(i % 2 === 0){
+            console.log( i,"IsLogged in")
+        }else{
+            console.log(i , "permission denied")
+        }
+    }
+
+
+})();
+
+
+
