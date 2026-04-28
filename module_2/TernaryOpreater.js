a412a7a1ec38be8db7035b487fce2b4f087b57f5
Prince1130-sam <princekuldeep2022@gmail.com>
2026-04-28 21:06:19 +0530

    ternary opreater

---
+++ module_2/TernaryOpreater.js
@@ -0,0 +1,11 @@
+let a = 10 ;
+let b = 20 ;
+let max ;
+
+max = (a < b)? true : false;       // is me first value = true value hoti hai or second value false value hoti hai 
+
+console.log(max)
+
+
+
+
