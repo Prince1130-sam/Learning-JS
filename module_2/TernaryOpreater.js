0d04d6ab96a17172a2f9f2b10151d24921402a14
Prince1130-sam <princekuldeep2022@gmail.com>
2026-04-29 21:16:54 +0530

    update ternary opreater

--- module_2/TernaryOpreater.js
+++ module_2/TernaryOpreater.js
@@ -7,5 +7,10 @@ max = (a < b)? true : false;       // is me first value = true value hoti hai or
 console.log(max)
 
 
+let isLogged = true
+let answer;
 
 
+answer = (isLogged === true)? "yess its Logged in": "No iis not Logged in" ,(isLogged === false)? "it will be inside tha application" : "it is not in side";
+
+console.log(answer)
