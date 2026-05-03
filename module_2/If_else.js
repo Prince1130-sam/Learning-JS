d2f9c9665b1cec7c2f0c3d97dc3f62ac5101322d
Prince1130-sam <princekuldeep2022@gmail.com>
2026-05-03 12:28:41 +0530

    if_else

--- module_2/If_else.js
+++ module_2/If_else.js
@@ -7,3 +7,18 @@ if(num === 0){
 }else{
     console.log(num)
 }
+
+
+
+const average = 29 ;
+if(average >= 55){
+    console.log("student is A+")
+
+}else if(average <= 55 && average >= 30){
+    console.log("the student gread is A ")
+}else if(average <=30){
+    console.log("the student is pass")
+
+}else{
+    console.log("student is fail")
+}
