6357f8bf92beafa0370071aab5f9ca600c066c21
Prince1130-sam <princekuldeep2022@gmail.com>
2026-05-30 22:45:28 +0530

    module_7

--- module_7/replace.js
+++ module_7/replace.js
@@ -5,7 +5,11 @@ const str = "pricne kumar yadav";
 const answer = str.replace("yadav", "singh") // phla aurgument search karta hai replaceing word ko  
                                             // dusare aurgument ma replaceing word hai jo change karna hai 
                                             
-const str = "apple, apple, apple"
+const str2 = "apple, apple, apple"     // jab bhi same word do ya do ka jada bar likha hogo tab uska phla word hi change hoga 
+console.log(str2)
+const answer2 = str2.replace("apple", "orange") 
+console.log(answer2)
+
 console.log(answer);
 
 console.log(str)   // but original string ko modify nahi karta hai 
