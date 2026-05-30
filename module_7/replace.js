aa4237b0d7e20323815bccb78dadfd0466cb8bf7
Prince1130-sam <princekuldeep2022@gmail.com>
2026-05-30 17:37:30 +0530

    module_7

--- module_7/replace.js
+++ module_7/replace.js
@@ -3,10 +3,13 @@
 const str = "pricne kumar yadav";
 
 const answer = str.replace("yadav", "singh") // phla aurgument search karta hai replaceing word ko  
-                                            // dusare aurgument ma replaceing word hai jo change karna hai                                            
+                                            // dusare aurgument ma replaceing word hai jo change karna hai 
+                                            
+const str = "apple, apple, apple"
 console.log(answer);
 
-console.log(str)   // but original string ko modify nahi karta hai
+console.log(str)   // but original string ko modify nahi karta hai 
+
 
 
 
