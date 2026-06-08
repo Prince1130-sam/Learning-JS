d8424626dce52e5862ecb5d0c5bd1f0ac90e84b7
Prince1130-sam <princekuldeep2022@gmail.com>
2026-06-08 10:04:15 +0530

    module_8
    Corrected comment regarding array length change after deletion.

--- module_8/Delete.js
+++ module_8/Delete.js
@@ -4,4 +4,4 @@ let remove = [10,20,30,40];
 delete remove[1] // index number se access karta hai or jo slot delete hoga uski jagha (1 empty item)show hoga
 
 console.log(remove);  // array me delete opreater ka use element ko delete karna ka liya use hota hai 
-console.log(remove.length)   // array ki length ki change in hoti hai kabhi bhi
+console.log(remove.length)   // array ki length ki change nahi hoti hai kabhi bhi
