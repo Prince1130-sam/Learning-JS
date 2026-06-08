57daeee4fcc83f74d7859a812ebd4cd9a5231296
Prince1130-sam <princekuldeep2022@gmail.com>
2026-06-08 13:18:18 +0530

    module_9

--- module_9/findLastIndex.js
+++ module_9/findLastIndex.js
@@ -3,7 +3,7 @@
 
 let car = [1,2,3,4,5,6,7,8,9]
 
-let value = car.findLastIndex((elem )=>{ // is method me array ki last index liya jata hai jo comdition lo match karta hai 
+let value = car.findLastIndex((elem )=>{ // is method me array ki last index liya jata hai jo condition ko match karta hai 
      return elem % 2 === 0 
 })  //  is method me undefined hone par -1 output aata hai
 
