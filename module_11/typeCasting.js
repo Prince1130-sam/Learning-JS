b1c40fd4ec6a2928fde7c7b5a255dbaf06b2f17c
Prince1130-sam <princekuldeep2022@gmail.com>
2026-06-15 22:44:57 +0530

    updated

---
+++ module_11/typeCasting.js
@@ -0,0 +1,20 @@
+// Type casting or type conversion 
+
+// there are two type of casting
+
+// 1. implicit (automatic)
+// 2. Explicit (manual)
+
+// implicit 
+
+  const a = "5"* 2;
+  console.log(a)   // converd in number by javascript automatic
+
+
+  // explicit 
+
+  let b = "123456";
+  b = Number(b)  // usko developer manuale karta hai .
+
+  console.log(b)
+
