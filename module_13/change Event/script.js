8934bd76c1299a3542636c2e1e2bb5520fc816c9
Prince1130-sam <princekuldeep2022@gmail.com>
2026-06-26 23:08:20 +0530

    change event

---
+++ module_13/change Event/script.js
@@ -0,0 +1,14 @@
+
+
+let input = document.getElementById("input")
+
+
+input.addEventListener("change",function(){
+
+    alert("input value",input.value)
+
+
+
+    console.log("this is change value",input.value);
+
+})
