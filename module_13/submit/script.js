15f1fb4a03a106b3a7f19573886681aac0056b3c
Prince1130-sam <princekuldeep2022@gmail.com>
2026-06-28 18:31:30 +0530

    submit

--- module_13/submit/script.js
+++ module_13/submit/script.js
@@ -3,7 +3,9 @@
 let form = document.getElementById("form")
 let input = document.getElementById("input")
 
-form.addEventListener("submit",()=>{
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();     // ya ek browser ka event hai 
+
     let ans = input.value
 
     console.log(ans)
