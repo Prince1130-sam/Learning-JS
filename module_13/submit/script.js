059da1aad5cf7d51d5cc47db72d1a670ff8b0c68
Prince1130-sam <princekuldeep2022@gmail.com>
2026-06-28 18:21:14 +0530

    submit

---
+++ module_13/submit/script.js
@@ -0,0 +1,10 @@
+
+
+let form = document.getElementById("form")
+let input = document.getElementById("input")
+
+form.addEventListener("submit",()=>{
+    let ans = input.value
+
+    console.log(ans)
+})
