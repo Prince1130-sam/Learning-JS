8aaf7e2a853c027bed80179dadb746b0497bcc20
Prince1130-sam <princekuldeep2022@gmail.com>
2026-07-05 14:21:33 +0530

    update

--- module_13/submit/script.js
+++ module_13/submit/script.js
@@ -4,7 +4,7 @@ let form = document.getElementById("form")
 let input = document.getElementById("input")
 
 form.addEventListener("submit",(e)=>{
-    e.preventDefault();     // ya ek browser ka event hai 
+    e.preventDefault();     // ya ek browser ka event hai  // ya browser me jo bar bar default bhavier run hota hai usko rokta hai
 
     let ans = input.value
 
